e008969ff4173d88db03f5d4617c291bd99b7ac8
nilufer <r.nilufer@oxmics.com>
2023-04-07 11:54:54 +0530

    commit 3

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,4 +1,5 @@
 
+import Card from './Card';
 import ExpenseItems from './ExpenseItems';
 
 import './Expenses.css';
@@ -6,8 +7,8 @@ import './Expenses.css';
 function Expenses(props) {
 
     return(
-      
-        <div className='expenses'>
+    
+     <Card className='expenses'>
      <ExpenseItems 
      title = {props.items[0].title}
       amount={props.items[0].amount}
@@ -27,7 +28,7 @@ function Expenses(props) {
      title = {props.items[3].title}
       amount={props.items[3].amount}
       date ={props.items[3].date}/>
-    </div>
+    </Card>
     )
 
 }
