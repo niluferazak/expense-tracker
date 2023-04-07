e6c5d977244500935292f9ae8f77b824c22522f2
nilufer <r.nilufer@oxmics.com>
2023-04-07 20:05:12 +0530

    committed !!

--- src/App.js
+++ src/App.js
@@ -32,10 +32,16 @@ function App() {
           date: new Date(2021, 5, 12),
         },
       ]
+
+      const addExpenseHandler=(expense)=>{
+
+        console.log(expense);
+
+      };
     
 return(
   <div>
-  <NewExpense/>
+  <NewExpense onAddExpense= {addExpenseHandler}/>
   <Expenses items={expenses}/>
  
   </div>
